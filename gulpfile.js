0cd23a3599a62536e97e8f731837e56950bc5363
cactis <cactis.lin@gmail.com>
2016-01-30 21:51:17 +0800

    migrate to haml/coffee/sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,46 @@ var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var changed = require('gulp-changed');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var watch = require('gulp-watch');
+var haml = require('gulp-ruby-haml');
+var coffee = require('gulp-coffee');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  assetsSass: ['./assets/sass/**/*.sass'],
+  assetsHaml: ['./assets/haml/**/*.haml'],
+  assetsCoffee: ['./assets/coffee/**/*.coffee']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('assets-haml', function () {
+  gulp.src('./assets/haml/**/*.haml')
+    .pipe(haml())
+    .pipe(changed('www', {hasChanged: changed.compareSha1Digest}))
+    .pipe(gulp.dest('./www'));
+});
+
+gulp.task('assets-sass', function () {
+  gulp.src('./assets/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(changed('www/css', {hasChanged: changed.compareSha1Digest}))
+    .pipe(gulp.dest('./www/css'));
+});
+
+gulp.task('assets-coffee', function () {
+  gulp.src('./assets/coffee/**/*.coffee')
+    .pipe(coffee({bare: true}))
+    .pipe(changed('www/js', {hasChanged: changed.compareSha1Digest}))
+    .pipe(gulp.dest('./www/js'));
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
-    .pipe(sass({
-      errLogToConsole: true
-    }))
+    .pipe(sass())
+    .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
@@ -29,6 +54,9 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.assetsSass, ['assets-sass']);
+  gulp.watch(paths.assetsHaml, ['assets-haml']);
+  gulp.watch(paths.assetsCoffee, ['assets-coffee']);
 });
 
 gulp.task('install', ['git-check'], function() {
@@ -50,3 +78,5 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
+
+gulp.task('default', ['watch']);
